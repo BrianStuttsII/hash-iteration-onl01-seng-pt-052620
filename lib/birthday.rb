49f9dc84a0c4aedb83d5c51774cacbdeccfad061
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-10 03:37:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,9 @@ def happy_birthday(birthday_kids)
 end
 
 
+def happy_birthday(birthday_kids)
+  
+  birthday_kids.each do |kids_name,age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
